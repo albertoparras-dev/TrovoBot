9f016d5e999054397f10c672605e5b6dceac3584
albertoparras-dev <66566819+albertoparras-dev@users.noreply.github.com>
2020-07-14 00:27:34 +0200

    Fix Bug | Say Command

--- plugins/say/say.js
+++ plugins/say/say.js
@@ -7,18 +7,18 @@ module.exports = {
     command: 'say',
     permissions: ['moderator', 'creator'],
     alias: ['say'],
-		settings: false,
+	settings: false,
     credits: "Made by Friext#6935",
-	execute(message, args, user, bot, event) {
+	execute(bot, data, modules) {
         /**IF THE ARGS ARE MISSING TELL TO THE USER THAT HE HAS TO PUT THE ARGS **/
-        if (args.length < 1) {
+        if (data.args.length < 1) {
 			bot.sendMessage("You have to put a message.");
             return;
         }
 
         /**IF THE USER IS MOD/CREATOR, SAYS THE THING HE SAYS **/
-        if (args[0].charAt(0)) {
-        bot.sendMessage(`${args.join(" ")}`);
+        if (data.args[0].charAt(0)) {
+        bot.sendMessage(`${data.args.join(" ")}`);
         }
     },
 };
